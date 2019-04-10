249a0c3de073fbe4bbb89c4786de9007ffe00aeb
TimeJunjie <you@example.com>
2019-04-10 17:31:08 +0800

    添加答案显示功能

--- 答题练习/pages/play/play.js
+++ 答题练习/pages/play/play.js
@@ -12,38 +12,63 @@ Page({
      current:0,//当前题号
      score:0,
      questionList:[],
-     items:questionData.questionList[0].items
+     nowQuestion:[],
+     multiIndex:[{checked:false},{checked:false},{checked:false},{checked:false}]
+    //  items:questionData.questionList[0].items
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getRandomQuestion();
-    console.log("items");
-    console.log(this.data.items[0]);
-    console.log("myquestion");
-   var  current=this.current;
+    this.getRandomQuestion();//获取随机问题序列
+    // console.log("items");
+    // console.log(this.data.item1);
+    //获取当前题号
+   var current=this.data.current;
+   //获取当前题号对应的题目，并更新
+   this.setData({nowQuestion:this.data.questionList[current]});//赋值nowQuestion
+   console.log("当前题号为"+current+"对应的nowQuestion为：")
+   console.log(this.data.nowQuestion);
    console.log("check:"+this.data.myChecked);
-    var myquestion = questionData.questionList[this.data.current].items
-    console.log(myquestion)
+    // var myquestion = questionData.questionList[this.data.current].items
+    // console.log(myquestion)
     // this.setData({ items: myquestion.items})
     
   
   
   },
 
- //选择选项函数
+ //单选和判断函数，选择选项函数
  selectItem(event){
-    // var myChecked = selectId;
-    console.log(event);
-
-    console.log("selectId:"+parseInt(event.currentTarget.dataset.selectid));
-    var selectId =parseInt(event.currentTarget.dataset.selectid);
-    this.setData({
-      myChecked: selectId,
-      currentValue: selectId
-    })
+   
+    // console.log("selectId:"+parseInt(event.currentTarget.dataset.selectid));
+    if(this.data.nowQuestion.type_id==1||this.data.nowQuestion.type_id==0){
+      //type_id为1与0时则进行下列赋值(单选或判断)
+      console.log("看看我有没有进入单选判断");
+      console.log("id"+this.data.nowQuestion.type_id);
+    var selectId =parseInt(event.currentTarget.dataset.selectid);//将携带的参数selectid赋值给selectId
+   
+        this.setData({//在data数据里更新myChecked的值和当前选择的值
+          myChecked: selectId,
+          currentValue: selectId
+        })
+    }else if(this.data.nowQuestion.type_id==2){
+      console.log("看看我有没有进入多选");
+       console.log("id"+this.data.nowQuestion.type_id);
+        var index =event.currentTarget.dataset.index;//获取当前索引
+        var selectId =parseInt(event.currentTarget.dataset.selectid);//获取当前选项id
+        var multiIndex = this.data.multiIndex;
+        multiIndex[index].checked=!multiIndex[index].checked;//单击取反
+        if(multiIndex[index].checked){//单击加减赋值
+          this.setData({myChecked:this.data.myChecked+selectId})
+        }else{this.setData({myChecked:this.data.myChecked-selectId})}
+        this.setData({//上传至视图层
+          multiIndex:multiIndex,
+          currentValue: selectId
+        })
+        
+    }
    },
   // 下一题next_question函数
   next_question(){
@@ -52,7 +77,7 @@ Page({
     var num = this.data.current;
     //获得正确答案
     var correct = this.data.questionList[num].answer; 
-    // 将 答案转换
+    // 将ABCD答案转换为数字
     var correctItem = 0; 
     switch(correct){
       case 'A':correctItem=1
@@ -63,30 +88,88 @@ Page({
       break;
       case 'D':correctItem=1000
       break;
+      case 'AB':correctItem=11
+      break;
+      case 'AC':correctItem=101
+      break;
+      case 'AD':correctItem=1001
+      break;
+      case 'ABC':correctItem=111
+      break;
+      case 'ABD':correctItem=1011
+      break;
+      case 'BCD':correctItem=1110
+      break;
+      case 'ABCD':correctItem=1111
+      break;
     }  
-    
+    //判断当前选择与答案是否相等
     if(this.data.currentValue===correctItem){
         this.setData({
-          score:this.data.score+1
+          //分数加2分
+          score:this.data.score+2
+          
         })
      
     }
     //下标加1进入下一题
-    if (this.data.current < this.data.questionList.length-1){
+      //判断是否最后一题,解决最后一题无限加分问题
+    if (this.data.current >= this.data.questionList.length - 1){
+      //为最后一题时，值清零，
+      this.setData({
+        //多选值清零
+        multiIndex:[{checked:false},{checked:false},{checked:false},{checked:false}],
+        myChecked: 0,
+        //当前选项值清0
+        currentValue: 0,
+        current:0
+      })
+      //弹出结果框
+      wx.showModal({
+        title: '答题结束',
+        content: '您的成绩为'+this.data.score,
+        success(res) {
+          if (res.confirm) {
+            wx.navigateTo({
+              url: "../begin/begin"
+            })
+            console.log('用户点击确定')
+          } else if (res.cancel) {
+            wx.navigateTo({
+              url: "../begin/begin"
+            })
+            console.log('用户点击取消')
+          }
+        }
+      })
+    } else if (this.data.current < this.data.questionList.length-1&&(this.data.myChecked!=0 )){
     this.setData({
+      //清零当前多选值
+      multiIndex:[{checked:false},{checked:false},{checked:false},{checked:false}],
       current: this.data.current + 1,
-      //选项清0
-      myChecked :0
+      //选项圆点清0
+      myChecked :0,
+      //当前选项值清0
+      currentValue:0,
+      
     })
-    
+    //获取更新后下一题current的值来更新当前题目
+    var current=this.data.current;
+    this.setData({
+      ////更新当前题目
+      nowQuestion:this.data.questionList[current]
+    })
+    console.log("当前题号为"+this.data.current+"对应的nowQuestion为：");
+    console.log(this.data.nowQuestion);
+   
     }
   },
   //获取随机问题数列
   getRandomQuestion(){
     //随机
     var randomQuestion = questionData.questionList;
-    console.log("未打乱前newQuestion");
-    console.log(randomQuestion);
+    // console.log("未打乱前newQuestion");
+    // console.log(randomQuestion);
     randomQuestion.sort(function () { return 0.5 - Math.random(); });
     console.log("打乱后newQuestion");
     console.log(randomQuestion);
--- 答题练习/questionData/questionData.js
+++ 答题练习/questionData/questionData.js
@@ -1,59 +1,12 @@
-var questionList = [{
-  type:"单选题",
-  question: "在文档上输出  'Hello World' 的正确 Javascript 语法是？",
-  
-  items: [
-    { value: "0", name: "('Hello World')", checked: false },
-    { value: "0", name: "Hello World", checked: false },
-    { value: "0", name: "response.write('Hello World')", checked: false },
-    { value: "1", name: "document.write('Hello World')", checked: false }
-  ],
-  answer:"B",
-}, 
-{
-  type: "单选题",
-  question: "这是第2题，AngularJS1中指定控制器的是哪个指令？",
-    items: [
-    { value: "0", name: "ng-route", checked: false },
-    { value: "0", name: "ng-template", checked: false },
-    { value: "0", name: "ng-model", checked: false },
-    { value: "1", name: "ng-controller", checked: false }
-  ],
-    answer: "B",},
-  {
-    type: "单选题",
-    question: "这是第三题，在文档上输出  'Hello World' 的正确 Javascript 语法是？",
-     items: [
-      { value: "0", name: "('Hello World')", checked: false },
-      { value: "0", name: "Hello World", checked: false },
-      { value: "0", name: "response.write('Hello World')", checked: false },
-      { value: "1", name: "document.write('Hello World')", checked: false }
-    ],
-    answer: "A",
-  }, 
-  {
-    type: "单选题",
-    question: "这是第四题在文档上输出  'Hello World' 的正确 Javascript 语法是？",
-     items: [
-      { value: "0", name: "('Hello World')", checked: false },
-      { value: "0", name: "Hello World", checked: false },
-      { value: "0", name: "response.write('Hello World')", checked: false },
-      { value: "1", name: "document.write('Hello World')", checked: false }
-    ],
-    answer: "A",
-    
-  }, 
-  {
-    type: "单选题",
-    question: "这是第五题在文档上输出  'Hello World' 的正确 Javascript 语法是？",
-    items: [
-      { value: "0", name: "('Hello World')", checked: false },
-      { value: "0", name: "Hello World", checked: false },
-      { value: "0", name: "response.write('Hello World')", checked: false },
-      { value: "1", name: "document.write('Hello World')", checked: false }
-    ],
-    answer: "A",
-  }
+var questionList = [{ "type_id": "2", "type": "多选题", "question": "1这是多选1对于服现役（    ）的人员，颁发银质国防服役纪念章。", "item1": "A、这是多选8年以内", "item2": "B、这是多选2满8年以上、不满16年", "item3": "C、这是多选满16年以上、不满30年", "item4": "D、这是多选满30年以上", "answer": "C" },
+  { "type_id": "2", "type": "多选题", "question": "2这是多选对于服现役（    ）的人员，颁发金质国防服役纪念章。", "item1": "A、这是多选8年以内", "item2": "B、这是多选满8年以上、不满16年", "item3": "C、这是多选满16年以上、不满30年", "item4": "D、这是多选满30年以上", "answer": "D" },
+  { "type_id": "0", "type": "单选题", "question": "3给烈士颁发（    ）。", "item1": "A、献身国防铜质纪念章", "item2": "B、献身国防银质纪念章", "item3": "C、国防服役金质纪念章", "item4": "D、献身国防金质纪念章", "answer": "D" },
+  { "type_id": "0", "type": "单选题", "question": "4给因公牺牲军人颁发（    ）。", "item1": "A、献身国防铜质纪念章", "item2": "B、献身国防银质纪念章", "item3": "C、国防服役银质纪念章", "item4": "D、献身国防金质纪念章", "answer": "B" },
+  { "type_id": "0", "type": "单选题", "question": "5给因公致残军人颁发（    ）。", "item1": "A、献身国防铜质纪念章", "item2": "B、献身国防银质纪念章", "item3": "C、国防服役铜质纪念章", "item4": "D、国防服役银质纪念章", "answer": "A" },
+  { "type_id": "0", "type": "单选题", "question": "6纪念章的颁发对象由（    ）以上单位政治工作部门逐级审核呈报。", "item1": "A、营级", "item2": "B、团级", "item3": "C、旅级", "item4": "D、军级", "answer": "B" },
+  { "type_id": "1", "type": "判断题", "question": "7严禁以部队番号、代号建立QQ、微信、微博等网络社群。（    ）", "item1": "", "item2": "", "item3": "", "item4": "", "answer": "A" },
+  { "type_id": "1", "type": "判断题", "question": "8非涉密计算机可以在军队内部网络和国际互联网之间交叉联接。（    ）", "item1": "", "item2": "", "item3": "", "item4": "", "answer": "B" },
+  { "type_id": "1", "type": "判断题", "question": "9可以将公网移动电话通过US错插口接入涉密计算机进行充电。（    ）", "item1": "", "item2": "", "item3": "", "item4": "", "answer": "B" }
 ]
 
 // 接口以对象并赋值的形式
